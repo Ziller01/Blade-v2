f19b03a0543d7010fcb48af9d9abf8432e12b5ab
Ziller <memo3rb2002@gmail.com>
2025-09-02 09:16:03 +0300

    Improved

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,30 @@ function toggleDarkMode() {
     // localStorage.theme = 'dark';
   }
 }
+
+
+function toggleShow(name) {
+  let btns = document.querySelectorAll(`#${name}-btn`);
+  let q = document.querySelectorAll(`#${name}`);
+  q.forEach(e => {
+    e.classList.toggle('md:hidden')
+  })
+  btns.forEach(b => {
+    b.classList.toggle('hidden')
+  })
+}
+
+function toggleList() {
+
+  let btn = document.getElementById(`nav-toggle`);
+  let list = document.getElementById(`nav-list`);
+
+  btn.classList.toggle('bi-x-lg')
+
+  if (list.classList.contains('max-lg:translate-y-0')) {
+    list.classList.remove('max-lg:translate-y-0')
+  } else {
+    list.classList.add('max-lg:translate-y-0')
+  }
+
+}
