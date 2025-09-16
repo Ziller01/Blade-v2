db3e26ae1761f8a7dc462500f8b651752670b803
Ziller <memo3rb2002@gmail.com>
2025-09-16 23:25:07 +0300

    Fixing ads issue

--- js/config.js
+++ js/config.js
@@ -4,11 +4,11 @@ let sections = [
         name: "Ads",
         name_ar: "اعلانات",
         links: [
-            './media/ads/(1).mp4',
-            './media/ads/(2).mp4',
-            './media/ads/(3).mp4',
-            './media/ads/(4).mp4',
-            './media/ads/(5).mp4',
+            '/media/adv/(1).mp4',
+            '/media/adv/(2).mp4',
+            '/media/adv/(3).mp4',
+            '/media/adv/(4).mp4',
+            '/media/adv/(5).mp4',
         ]
     },
     {
@@ -16,16 +16,16 @@ let sections = [
         name: "Fashion",
         name_ar: "موضة",
         links: [
-            './media/fashion/(1).mp4',
-            './media/fashion/(2).mp4',
-            './media/fashion/(3).mp4',
-            './media/fashion/(4).mp4',
-            './media/fashion/(5).mp4',
-            './media/fashion/(6).mp4',
-            './media/fashion/(7).mp4',
-            './media/fashion/(8).mp4',
-            './media/fashion/(9).mp4',
-            './media/fashion/(10).mp4',
+            '/media/fashion/(1).mp4',
+            '/media/fashion/(2).mp4',
+            '/media/fashion/(3).mp4',
+            '/media/fashion/(4).mp4',
+            '/media/fashion/(5).mp4',
+            '/media/fashion/(6).mp4',
+            '/media/fashion/(7).mp4',
+            '/media/fashion/(8).mp4',
+            '/media/fashion/(9).mp4',
+            '/media/fashion/(10).mp4',
         ]
     },
     {
@@ -33,12 +33,12 @@ let sections = [
         name: "Food",
         name_ar: "طعام",
         links: [
-            './media/food/(1).mp4',
-            './media/food/(2).mp4',
-            './media/food/(3).mp4',
-            './media/food/(4).mp4',
-            './media/food/(5).mp4',
-            './media/food/(6).mp4',
+            '/media/food/(1).mp4',
+            '/media/food/(2).mp4',
+            '/media/food/(3).mp4',
+            '/media/food/(4).mp4',
+            '/media/food/(5).mp4',
+            '/media/food/(6).mp4',
         ]
     },
     {
@@ -46,11 +46,11 @@ let sections = [
         name: "Gym",
         name_ar: "جيم",
         links: [
-            './media/gym/(1).mp4',
-            './media/gym/(2).mp4',
-            './media/gym/(3).mp4',
-            './media/gym/(4).mp4',
-            './media/gym/(5).mp4',
+            '/media/gym/(1).mp4',
+            '/media/gym/(2).mp4',
+            '/media/gym/(3).mp4',
+            '/media/gym/(4).mp4',
+            '/media/gym/(5).mp4',
         ]
     },
     {
@@ -58,12 +58,12 @@ let sections = [
         name: "Meetings",
         name_ar: "لقائات",
         links: [
-            './media/street/(1).mp4',
-            './media/street/(2).mp4',
-            './media/street/(3).mp4',
-            './media/street/(4).mp4',
-            './media/street/(5).mp4',
-            './media/street/(6).mp4',
+            '/media/street/(1).mp4',
+            '/media/street/(2).mp4',
+            '/media/street/(3).mp4',
+            '/media/street/(4).mp4',
+            '/media/street/(5).mp4',
+            '/media/street/(6).mp4',
         ]
     },
     {
@@ -71,14 +71,14 @@ let sections = [
         name: "Story",
         name_ar: "قصص",
         links: [
-            './media/story/(1).mp4',
-            './media/story/(2).mp4',
-            './media/story/(3).mp4',
-            './media/story/(4).mp4',
-            './media/story/(5).mp4',
-            './media/story/(6).mp4',
-            './media/story/(7).mp4',
-            './media/story/(8).mp4',
+            '/media/story/(1).mp4',
+            '/media/story/(2).mp4',
+            '/media/story/(3).mp4',
+            '/media/story/(4).mp4',
+            '/media/story/(5).mp4',
+            '/media/story/(6).mp4',
+            '/media/story/(7).mp4',
+            '/media/story/(8).mp4',
         ]
     },
     {
@@ -86,13 +86,13 @@ let sections = [
         name: "More",
         name_ar: "المزيد",
         links: [
-            './media/more/(1).mp4',
+            '/media/more/(1).mp4',
         ]
     },
 ]
 
 let reviewsData = [
-    '../media/reviews/(1).png',
-    '../media/reviews/(2).png',
-    '../media/reviews/(3).png',
+    '/media/reviews/(1).png',
+    '/media/reviews/(2).png',
+    '/media/reviews/(3).png',
 ]
